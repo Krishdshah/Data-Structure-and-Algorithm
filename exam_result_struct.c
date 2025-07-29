1c0456a33313482f45da2d0445f4d381137672ba
Krishdshah <kds1592006@gmail.com>
2025-07-30 00:31:05 +0530

    Struct based C programs

---
+++ exam_result_struct.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <string.h>
+
+#define stud 10
+
+struct subjects {
+    int marks;
+};
+
+struct student {
+    int reg_no;
+    char name[30];
+    struct subjects s1[5];
+    int student_total;
+};
+
+struct classes {
+    char sec_no[10];
+    struct student stud_array[stud];
+    int class_highest;
+    char topper_cls[30];
+};
+
+struct department {
+    char dept_name[30];
+    struct classes class_array[10];
+    int num_classes;
+    int dept_highest;
+    char topper_dept[30];
+};
+
+int main() {
+    int i, j, k, l, deptNo;
+
+    printf("Enter total number of departments in institute:\n");
+    scanf("%d", &deptNo);
+    struct department dept_array[deptNo];
+
+    for(i = 0; i < deptNo; i++) {
+        printf("Enter dept name:\n");
+        scanf("%s", dept_array[i].dept_name);
+
+        printf("Enter total no of classes in this department:\n");
+        scanf("%d", &dept_array[i].num_classes);
+
+        dept_array[i].dept_highest = 0;
+
+        for(j = 0; j < dept_array[i].num_classes; j++) {
+            dept_array[i].class_array[j].class_highest = 0;
+
+            printf("Enter class sec_no:\n");
+            scanf("%s", dept_array[i].class_array[j].sec_no);
+
+            for(k = 0; k < stud; k++) {
+                printf("Enter roll no of student %d:\n", k + 1);
+                scanf("%d", &dept_array[i].class_array[j].stud_array[k].reg_no);
+
+                printf("Enter name of student %d:\n", k + 1);
+                scanf("%s", dept_array[i].class_array[j].stud_array[k].name);
+
+                dept_array[i].class_array[j].stud_array[k].student_total = 0;
+
+                for(l = 0; l < 5; l++) {
+                    printf("Enter marks of student %d for subject %d:\n", k + 1, l + 1);
+                    scanf("%d", &dept_array[i].class_array[j].stud_array[k].s1[l].marks);
+                    dept_array[i].class_array[j].stud_array[k].student_total += dept_array[i].class_array[j].stud_array[k].s1[l].marks;
+                }
+
+                if(dept_array[i].class_array[j].class_highest < dept_array[i].class_array[j].stud_array[k].student_total) {
+                    dept_array[i].class_array[j].class_highest = dept_array[i].class_array[j].stud_array[k].student_total;
+                    strcpy(dept_array[i].class_array[j].topper_cls, dept_array[i].class_array[j].stud_array[k].name);
+                }
+            }
+
+            if(dept_array[i].dept_highest < dept_array[i].class_array[j].class_highest) {
+                dept_array[i].dept_highest = dept_array[i].class_array[j].class_highest;
+                strcpy(dept_array[i].topper_dept, dept_array[i].class_array[j].topper_cls);
+            }
+        }
+    }
+
+    for(i = 0; i < deptNo; i++) {
+        printf("Department name: %s\n", dept_array[i].dept_name);
+        for(j = 0; j < dept_array[i].num_classes; j++) {
+            printf("Class Name: %s\n", dept_array[i].class_array[j].sec_no);
+            for(k = 0; k < stud; k++) {
+                printf("Name of Student: %s and Marks: %d\n",
+                    dept_array[i].class_array[j].stud_array[k].name,
+                    dept_array[i].class_array[j].stud_array[k].student_total);
+            }
+            printf("Topper of %s %s is %s, Total Marks = %d\n\n",
+                dept_array[i].dept_name,
+                dept_array[i].class_array[j].sec_no,
+                dept_array[i].class_array[j].topper_cls,
+                dept_array[i].class_array[j].class_highest);
+        }
+        printf("Topper of %s is %s, Total Marks = %d\n\n",
+            dept_array[i].dept_name,
+            dept_array[i].topper_dept,
+            dept_array[i].dept_highest);
+    }
+
+    return 0;
+}
---
+++ student_topper_struct.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+struct students {
+	int roll;
+	char name[30];
+	int sub1_marks;
+	int sub2_marks;
+	int sub3_marks;
+	int total_marks;
+};
+
+int main() {
+	struct students stud_list[5];
+	int i;
+	
+	for (i = 0; i < 5; i++) {
+		printf("Enter roll no of student %d:\n", i + 1);
+		scanf("%d", &stud_list[i].roll);
+
+		printf("Enter name of student %d:\n", i + 1);
+		scanf("%s", stud_list[i].name);
+
+		printf("Enter marks of student %d for subject 1:\n", i + 1);
+		scanf("%d", &stud_list[i].sub1_marks);
+
+		printf("Enter marks of student %d for subject 2:\n", i + 1);
+		scanf("%d", &stud_list[i].sub2_marks);
+
+		printf("Enter marks of student %d for subject 3:\n", i + 1);
+		scanf("%d", &stud_list[i].sub3_marks);
+
+		stud_list[i].total_marks = stud_list[i].sub1_marks + stud_list[i].sub2_marks + stud_list[i].sub3_marks;
+		printf("\n");
+	}
+
+	int highest = -1;
+	char topper[30];
+
+	for (i = 0; i < 5; i++) {
+		if (stud_list[i].total_marks > highest) {
+			highest = stud_list[i].total_marks;
+			strcpy(topper, stud_list[i].name);
+		}
+	}
+
+	printf("The result is: the topper of class is %s who scored %d.\n", topper, highest);
+	
+	// code by Krish D Shah (RA2411026010004) AB1
+	return 0;
+}
